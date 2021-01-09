f245b12eedb12e7340e5da2b600204f6fba3b4fe
Matriculus <pradeeshvishnu@outlook.com>
2021-01-09 13:45:26 +0530

    Added script and description

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # winusb
 Script to create Windows 10 bootable USB from linux
+
+This script works for Ubuntu and can be modified for any other distro by changing the $apt$ package to $rpm$, $zippy$, etc.
---
+++ winusb.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+cd ~/Downloads
+sudo apt install wimtools -y
+sudo mkdir /media/iso
+sudo mount -o loop $1 /media/iso
+scp /media/iso/sources/install.wim ~/Downloads
+wimlib-imagex split install.wim install.swm 4000
+rsync -avr --exclude='sources/install.wim' /media/iso/ $2/
+scp install*swm $2/sources/
+sudo umount /media/iso
+sudo rmdir /media/iso
