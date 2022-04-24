bc0ea4ce5f7cb6f0ec1880beb0bd6c968913a74d
Vishnu <pradeeshvishnu@outlook.com>
2022-04-24 11:39:14 +0100

    Adds correct path for install files while editing

--- winusb.sh
+++ winusb.sh
@@ -14,18 +14,18 @@ scp -v /media/iso/sources/install.wim ~/Downloads && \
 echo "Copying completed successfully!" && \
 echo "" && \
 echo "Splitting the installation for FAT32 partition..." && \
-wimlib-imagex split install.wim install.swm 4000 && \
+wimlib-imagex split ~/Downloads/install.wim ~/Downloads/install.swm 4000 && \
 echo "Splitting completed sccessfully!" && \
 echo "" && \
 echo "Copying the ISO contents to USB" && \
 rsync -avr --exclude='sources/install.wim' /media/iso/ $2 && \
-scp -v install*swm $2sources/ && \
+scp -v ~/Downloads/install*swm $2sources/ && \
 echo "Copying completed successfully!" && \
 echo "" && \
 echo "Cleaning up..." && \
 sudo umount /media/iso && \
 sudo rmdir /media/iso && \
-rm -f install.wim install*.swm && \
+rm -f ~/Downloads/install.wim ~/Downloads/install*.swm && \
 echo "Bootable USB creation successful!" && \
 echo "Enjoy!" && \
 echo "Please give a star to github.com/matriculus/winusb !" && \
